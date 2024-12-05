2ea62a8e12fa92bb326162e021ff121bab344861
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-05 17:58:04 +0700

    Add files via upload
    Homework = 12
    Hours_Lost = 1.5
    Course = 'Python'
    Time_spent_on_one_task = Hours_Lost/Homework
    prin

---
+++ new.py
@@ -0,0 +1,5 @@
+Homework = 12
+Hours_Lost = 1.5
+Course = 'Python'
+Time_spent_on_one_task = Hours_Lost/Homework
+print('Курс:', Course,',','всего задач:',Homework,',','затрачено часов:',Hours_Lost,',','среднее время выполнения:',Time_spent_on_one_task,'часа.' )
