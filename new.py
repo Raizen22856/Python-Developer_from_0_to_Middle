ab62df08a9327655ab33c42df518a3b6abe08b45
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-05 18:01:35 +0700

    Delete new.py

--- new.py
+++
@@ -1,5 +0,0 @@
-Homework = 12
-Hours_Lost = 1.5
-Course = 'Python'
-Time_spent_on_one_task = Hours_Lost/Homework
-print('Курс:', Course,',','всего задач:',Homework,',','затрачено часов:',Hours_Lost,',','среднее время выполнения:',Time_spent_on_one_task,'часа.' )
